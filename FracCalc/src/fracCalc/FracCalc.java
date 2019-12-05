bc04fb1207a7e10f44b335785e0127cfa18605e0
heffleth000 <heffleth000@CAM-LAB-23.bgsd.k12.wa.us>
2019-12-05 07:52:27 -0800

    Almost done with checkpoint 1; need to break down fraction into parts

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -1,3 +1,10 @@
+/* CHECKPOINT 1:
+ * inside produceAnswer() find the length of the calculation
+ * find indexOf the second space in the calculation
+ * stringLength = length;	x = indexOf(" ") + 3;	substring(y, length);
+ */
+
+
 package fracCalc;
 import java.util.*;
 
@@ -7,8 +14,8 @@ public class FracCalc {
     	Scanner userInput = new Scanner (System.in);
     	System.out.print("What calculation should be performed? ");
     	String calc = userInput.nextLine();
-    	//produceAnswer(calc);
-    	System.out.print(produceAnswer(calc));
+    	produceAnswer(calc);
+    	System.out.print(produceAnswer(calc)); //prints out the second fraction
     	
     	// TODO: Read the input from the user and call produceAnswer with an equation
     }
@@ -22,8 +29,15 @@ public class FracCalc {
     // The function should return the result of the fraction after it has been calculated
     //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input) {
-    	String secondFrac = input.substring(8, 13);
-        
+    	int length = input.length(); //length of input; is end of string
+    	int space = input.indexOf(" "); //index of first space
+    	int x = space + 3; //index of fraction after operator
+    	String secondFrac = input.substring(x, length); //finds the second fraction
+    	
+    	String firstFrac = input.substring(0, space); //finds the first fraction
+    	
+    	String operator = input.substring(space, space + 2); //finds the operator
+    	
         return secondFrac;
     }
 
--- FracCalc/src/fracCalc/FracCalcTestCheckpoint1.java
+++ FracCalc/src/fracCalc/FracCalcTestCheckpoint1.java
@@ -1,11 +1,33 @@
 package fracCalc;
-
+import java.util.*;
 import org.junit.Test;
 
 // Checkpoint 1-only tests
-public class FracCalcTestCheckpoint1
-{
+public class FracCalcTestCheckpoint1 {
     @Test public void testCheckpoint1_1() {  FracCalcTestALL.assertForEarlyCheckpoints("6_5/8", "whole:6 numerator:5 denominator:8", "12_3/8", FracCalc.produceAnswer("5_3/4 + 6_5/8"));}
     @Test public void testCheckpoint1_2() {FracCalcTestALL.assertForEarlyCheckpoints("20", "whole:20 numerator:0 denominator:1", "-20_3/7", FracCalc.produceAnswer("-3/7 - 20"));}
     @Test public void testCheckpoint1_3() {FracCalcTestALL.assertForEarlyCheckpoints("27/21", "whole:0 numerator:27 denominator:21", "-33_2/7", FracCalc.produceAnswer("-32 - 27/21"));}
+	
+    public static void main(String[] args) {
+    	Scanner userInput = new Scanner (System.in);
+    	System.out.print("What calculation should be performed? ");
+    	String calc = userInput.nextLine();
+    	produceAnswer(calc);
+    	System.out.print(produceAnswer(calc)); //prints out the second fraction    	
+    }
+ 
+    public static String produceAnswer(String input) {
+    	int length = input.length(); //length of input; is end of string
+    	int space = input.indexOf(" "); //index of first space
+    	int x = space + 3; //index of fraction after operator
+    	String secondFrac = input.substring(x, length); //finds the second fraction
+    	
+    	String firstFrac = input.substring(0, space); //finds the first fraction
+    	
+    	String operator = input.substring(space, space + 2); //finds the operator
+    	
+        return secondFrac;
+    }
+
 }
+
