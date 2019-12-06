bcbeb7a0c9e66f0e68e0a501ad58b924d80a1d2a
heffleth000 <heffleth000@CAM-LAB-23.bgsd.k12.wa.us>
2019-12-06 07:51:56 -0800

    Done with Checkpoint 1. Now breaking down second fraction into parts

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -2,8 +2,16 @@
  * inside produceAnswer() find the length of the calculation
  * find indexOf the second space in the calculation
  * stringLength = length;	x = indexOf(" ") + 3;	substring(y, length);
+ * firstFrac = input.substring(0, space);	operator = input.substring(space, space + 2);
+ *
+ * CHECKPOINT 2:
+ * in main method, ask for userInput until "quit" is typed
+ * in produceAnswer() break up each fraction into whole number, numerator, and denominator
+ * use if statement about underscores and slashes "/"
  */
 
+// Equations to test:
+// 1_2/3 + 4_5/6		5_3/4 - 6_5/8		-3/7 - 20		-32 - 27/21
 
 package fracCalc;
 import java.util.*;
@@ -12,27 +20,34 @@ public class FracCalc {
 	
     public static void main(String[] args) {
     	Scanner userInput = new Scanner (System.in);
-    	System.out.print("What calculation should be performed? ");
+    	System.out.print("What calculation should be performed? (type \"quit\" to stop) ");
     	String calc = userInput.nextLine();
-    	produceAnswer(calc);
-    	System.out.print(produceAnswer(calc)); //prints out the second fraction
+    	if (calc.equals("quit")) {
+    		System.out.println("Program stopped");	//stops program
+    	 }
+    	while (!calc.equals("quit")) {
+    		produceAnswer(calc);
+        	System.out.println(produceAnswer(calc)); //prints out the second fraction
+        	System.out.print("What calculation should be performed? (type \"quit\" to stop) ");
+        	calc = userInput.nextLine();
+        	if (calc.equals("quit")) {		//stops program
+        		System.out.println("Program stopped");
+        	 }
+    	}	
     	
-    	// TODO: Read the input from the user and call produceAnswer with an equation
     }
     
-    // ** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
-    // This function takes a String 'input' and produces the result
-    //
-    // input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
-    //      e.g. input ==> "1/2 + 3/4"
-    //        
-    // The function should return the result of the fraction after it has been calculated
-    //      e.g. return ==> "1_1/4"
+    
     public static String produceAnswer(String input) {
     	int length = input.length(); //length of input; is end of string
     	int space = input.indexOf(" "); //index of first space
     	int x = space + 3; //index of fraction after operator
     	String secondFrac = input.substring(x, length); //finds the second fraction
+    	
+    	int underscore = secondFrac.indexOf("_");
+    	if (underscore == -1) {
+    		
+    	}
     	
     	String firstFrac = input.substring(0, space); //finds the first fraction
     	
@@ -44,3 +59,11 @@ public class FracCalc {
     // TODO: Fill in the space below with any helper methods that you think you will need
     
 }
+//** IMPORTANT ** DO NOT DELETE THIS FUNCTION.  This function will be used to test your code
+// This function takes a String 'input' and produces the result
+//
+// input is a fraction string that needs to be evaluated.  For your program, this will be the user input.
+//      e.g. input ==> "1/2 + 3/4"
+//        
+// The function should return the result of the fraction after it has been calculated
+//      e.g. return ==> "1_1/4"
